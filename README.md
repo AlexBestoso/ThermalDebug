643f848428ef56c968f098000a583f655f98dbad
Morning Star <alexbestoso@gmail.com>
2026-01-20 10:03:41 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ThermalDebug
-C++ code used to debug advanced bitwise operations and other binary errors.
+# ThermalDebug - Work In Progress
+
+description:
+This class and program work to gether to generate a "thermel exposure dump" or .ted file out of the programs that you're working on.
+
+A .ted file in this instance is a yet to be defined format.
+
+This code will allow programmers to swap out the bitwise operations in their applications and generate a series of binary files.
+
+These binary files will then be loadable into the main thermalDebug program, where you will be able to use the keyboard to cycle through your 
+algorithm per binary operation. 
+
+The goal is to make it so that I can compair an encode stream with a decode stream and see where the process gets desynchronized.
---
+++ thermalDebug.class.h
@@ -0,0 +1,82 @@
+
+class ThermalDebug{
+	private:
+		void color_reset(void){printf("\033[0m");}
+		
+		void color_bright(void){printf("\033[1m");}
+		void color_underline(void){printf("\033[4m");}
+		void color_flashing(void){printf("\033[5m");}
+
+		void color_blackFg(void){printf("\033[30m");}
+		void color_redFg(void){printf("\033[31m");}
+		void color_greenFg(void){printf("\033[32m");}
+		void color_yellowFg(void){printf("\033[33m");}
+		void color_blueFg(void){printf("\033[34m");}
+		void color_purpleFg(void){printf("\033[35m");}
+		void color_cyanFg(void){printf("\033[36m");}
+		void color_whiteFg(void){printf("\033[37m");}
+		
+		void color_blackBg(void){printf("\033[40m");}
+		void color_redBg(void){printf("\033[41m");}
+		void color_greenBg(void){printf("\033[42m");}
+		void color_yellowBg(void){printf("\033[43m");}
+		void color_blueBg(void){printf("\033[44m");}
+		void color_purpleBg(void){printf("\033[45m");}
+		void color_cyanBg(void){printf("\033[46m");}
+		void color_whiteBg(void){printf("\033[47m");}
+	
+		int logFd=-1;
+		std::string executionLog = "";
+		std::string logLocation = "./thermal.log";
+		std::string logCurrent="";
+		bool freshLog(void){
+			logFd = open(logLocation.c_str(), O_CREAT|O_TRUNC, S_IRUSR | S_IWUSR);
+			if(logFd == -1){
+				printf("[E]-~ failed to open log file.\n");
+				return false;
+			}
+			close(logFd);
+			return true;
+		}
+		bool lprint(std::string msg, size_t msgSize){
+			for(int i=0; i<msgSize; i++) printf("%c", msg[i]);
+			return this->log(msg, msgSize);
+		}
+		bool log(std::string data, size_t dataSize){
+			logFd = open(logLocation.c_str(), O_APPEND | O_RDWR);
+			if(logFd == -1){
+				printf("[E]-~ failed to open log file.\n");
+				return false;
+			}
+			if(write(logFd, data.c_str(), dataSize) != dataSize){
+				printf("[E]-~ failed write log message.\n");
+				close(logFd);
+				return false;
+			}
+			close(logFd);
+			return true;
+		} 
+	public:
+		std::string binaryString(int src, int bitCount, bool invFormat){
+			std::string dst = "";
+			if(!invFormat){
+				for(int i=0; i<bitCount && i<(sizeof(int)*8); i++){
+					dst += (src & 1) == 0 ? '0' : '1';
+					src >>= 1;
+				}
+			}else{
+				for(int i=bitCount-1; i>=0 && i<(sizeof(int)*8); i--){
+					dst += ((src >> i )&(src & 1)) == 0 ? '0' : '1';
+				}
+
+			}
+			return dst;
+		}
+
+		int thermalAnd(){}
+		int thermalOr(){}
+		int thermalXor(){}
+		int thermalNot(){}
+		int thermalRightshift(){}
+		int thermalLeftshift(){}
+}thermalDbg;
